2d3efc64492a7478dd1d789d3a3d335b52443b6a
Khanh Hoa <khanhhoa3997@gmail.com>
2021-10-05 16:57:11 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  List text =['DO','RE','MI','FA','SON','LA','SI'];
+  String _do = 'DO';
+  String _re = 'RE';
+  String _mi = 'MI';
+  String _fa = 'FA';
+  String _son = 'SON';
+  String _la = 'LA';
+  String _si = 'SI';
+  final AudioCache _audioCache = AudioCache(); 
 
+  void btnTapped(){
+    if(text[0]==_do)
+    {
+      _audioCache.play('audios/noteDo.wav');
+    }
+    else if(text[1] == _re)
+    {
+      _audioCache.play('audios/noteRE.wav');
+    }
+    else if(text[2] == _mi)
+    {
+      _audioCache.play('audios/noteMI.wav');
+    }
+    else if(text[3] == _fa)
+    {
+      _audioCache.play('audios/noteFA.wav');
+    }
+    else if(text[4] == _son)
+    {
+      _audioCache.play('audios/noteSON.wav');
+    }
+    else if(text[5] == _la)
+    {
+      _audioCache.play('audios/noteLA.wav');
+    }
+    else if(text[6] == _si)
+    {
+      _audioCache.play('audios/noteSI.wav');
+    }
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      title: 'Xylophone',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Xylophone'),
+          backgroundColor: Colors.teal[400],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_do),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(100, 100),
+                        backgroundColor: Colors.red,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_re),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(95, 95),
+                        backgroundColor: Colors.yellow,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_mi),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(90, 90),
+                        backgroundColor: Colors.green,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(40, 0, 40, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_fa),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(85, 85),
+                        backgroundColor: Colors.pink,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(50, 0, 50, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_son),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(80, 80),
+                        backgroundColor: Colors.purple,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(60, 0, 60, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_la),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(75, 75),
+                        backgroundColor: Colors.orange,
+                      ),
+                    ),
+                  ),
+                )
+              ),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(70, 0, 70, 10),
+                    child: ElevatedButton(
+                      onPressed: btnTapped,
+                      child: Text(_si),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(70, 70),
+                        backgroundColor: Colors.blue,
+                      ),
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),),
+        ),
     );
   }
 }
